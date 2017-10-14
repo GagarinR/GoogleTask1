4140d42a7e92eda6d91fab485893e4c921ed6106
TheRutubeify <elitegroup.kz@gmail.com>
2017-10-13 18:32:25 -0700

    Update GoogleTask.py

--- GoogleTask.py
+++ GoogleTask.py
@@ -3,39 +3,7 @@
 import re
 import math
 
-def answer1(n):
-    n = '2357111317192329313741434753596167717379838997101103107109113127131137139149151157163167173179181191193197199'
-    primal_num = n  # string
-    matched_list = []
 
-
-
-
-    for i in range(0, 11):
-
-        print '~~~~~~ %d ~~~~~~' % i
-        print i
-        if i == 0:
-            print '23571'
-        else:
-            prefix_num = 5 - len(str(i))
-
-            pattern = re.compile(r'\d{%d}%d' % (prefix_num, i))
-            match = pattern.findall(primal_num)
-
-            if match:
-                print 'match: ', match, i
-
-                locals()
-                primal_num = primal_num.replace(match[0], '')
-                matched_list.extend(match)
-
-                return match[0]
-
-            else:
-                return -1
-
-                # print matched_list
 
 def answer(n):
     #pnum = '2357111317192329313741434753596167717379838997101103107109113127131137139149151157163167173179181191193197199'
